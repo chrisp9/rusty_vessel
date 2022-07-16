15a57988d8b4cf6c2cb56950bb45ffa27569748b
chris_p9 <chris_powell9@outlook.com>
2022-07-16 18:18:19 +0100

    Sugar

--- src/container.rs
+++ src/container.rs
@@ -13,28 +13,32 @@ impl Container {
         return Container {root: path, current_chunk: None, chunk_capacity: capacity};
     }
 
+    pub fn get_current_chunk(&mut self, chunk: &mut OpenChunk, index: u64) -> &mut OpenChunk {
+        match chunk.is_full(self.chunk_capacity) {
+            true => chunk,
+            false => self.current_chunk.insert(
+                OpenChunk::create_new(self.root.clone, index))
+        }
+    }
+
     pub fn write(&mut self, index: u64, data: String) {
         let this_chunk = self.current_chunk.get_or_insert_with(|| {
 
-            // Try to open the latest chunk, if it exists.
             let latest_chunk = OpenChunk::open_latest(self.root.clone());
 
             return match latest_chunk {
                 Some(c) => c,
 
                 // If there is no latest chunk, create a new genesis chunk
-                None => OpenChunk::create_new(self.root.clone(),index)
+                None => OpenChunk::create_new(self.root.clone(), index)
             };
         });
 
-        if !this_chunk.is_full(self.chunk_capacity) {
-            this_chunk.write(index, &data);
-        } else {
-            let chunk = self.current_chunk.insert(
-                OpenChunk::create_new(self.root.clone(), index));
-
-            chunk.write(index, &data);
-        }
+        // If the current chunk is not full, write to it,
+        // otherwise create a new chunk and write to the
+        // new chunk.
+        self.get_current_chunk(this_chunk, index)
+            .write(index, &data);
     }
 
     pub fn is_full(&self) -> bool {
